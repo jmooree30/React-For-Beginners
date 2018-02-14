5f5a9b90594c4fa274711c77810f1e67f3fb7a52
Jacob <jmooree30@gmail.com>
2018-02-13 22:43:18 -0500

    React Events

---
+++ src/components/StorePicker.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { getFunName } from "../helpers";
+
+class StorePicker extends React.Component {
+  goToStore() {
+
+  }
+  render() {
+    return (
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please Enter A Store</h2>
+        <input
+          type="text"
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Visit Store</button>
+      </form>
+    );
+  }
+}
+
+export default StorePicker;
