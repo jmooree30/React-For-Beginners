54200b52041d85ad5a48d56d15cf0415373ee4f8
Jacob <jmooree30@gmail.com>
2018-02-17 18:02:49 -0500

    localStorage

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from "re-base";
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyBNhH6cY_MsFcH3ZXbxFRDOxfVpmGcFkF8",
+    authDomain: "catch-of-the-day-jacob-moore.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-jacob-moore.firebaseio.com",
+});
+
+export default base;
--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Order from "./Order";
 import Inventory from "./Inventory";
 import sampleFishes from "../sample-fishes";
 import Fish from "./Fish";
+import base from "../base";
 
 class App extends React.Component {
   constructor() {
@@ -15,6 +16,30 @@ class App extends React.Component {
       fishes: {},
       order: {}
     };
+  }
+
+  componentWillMount() {
+    this.ref = base.syncState(`${this.props.params.storeId}/fishes`
+      , {
+        context: this,
+        state: 'fishes'
+      })
+    const localStorageRef = localStorage.getItem(`order-${this.
+      props.params.storeId}`)
+    if (localStorageRef) {
+      this.setState({
+        order: JSON.parse(localStorageRef)
+      })
+    }
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
+
+  componentWillUpdate(nextProps,nextState) {
+    localStorage.setItem(`order-${this.props.params.storeId}`,
+      JSON.stringify(nextState.order));
   }
 
   addFish(fish) {
@@ -50,7 +75,10 @@ class App extends React.Component {
           }
           </ul>
         </div>
-        <Order />
+        <Order 
+          fishes = {this.state.fishes} 
+          order = {this.state.order}
+          params = {this.props.params} />
         <Inventory addFish={this.addFish} loadSamples = {this.loadSamples} />
       </div>
     );
